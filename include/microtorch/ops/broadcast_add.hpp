911371f00da30a1769b829f7044f3faa53d7c575
utsabrajbhattarai <utsabdai000@gmail.com>
2026-07-07 10:13:38 +0545

    Test: Added and verified the test for working of broadcast_add

--- include/microtorch/ops/broadcast_add.hpp
+++ include/microtorch/ops/broadcast_add.hpp
@@ -4,7 +4,7 @@
 
 namespace microtorch {
 
-//broadcast add: matrix addition usually for biases or residuals
+//broadcast add: matrix addition usually for biases of a specific broadcasting rule
 //note that parameters are actually TensorPtr& cause inside engine we will always be dealing with sharedptr so to address the graph and parents properly cause bare Tensor& is justa. stack copy of value and doesnt work with our backward() implementation
 TensorPtr broadcast_add(const TensorPtr& a, const TensorPtr& b);
 
