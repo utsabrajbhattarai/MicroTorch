3d46b11f86dfc798d273d843dee8f636636e548b
Sonik Bhusal <sonikbhusal6655@gmail.com>
2026-07-11 20:01:55 +0545

    Add CSV to Eigen Matrix data loader

--- include/microtorch/data_loader.hpp
+++ include/microtorch/data_loader.hpp
@@ -1,8 +1,17 @@
-#pragma once
+#pragma once  // if this header file is included multiple times, it will only be included once, avoiding redefinition errors
 #include <Eigen/Dense>  //for Eigen::MatrixXd
 #include <string>     // for std::string
+#include <vector>  // for vector
+#include <unordered_map> // for unordered_map
+#include <utility> // for std::pair
 
 // created microtorch namespace including load_csv function 
 namespace microtorch{
-    Eigen::MatrixXd load_csv(const std::string& path);
+    Eigen::MatrixXd load_csv(const std::string& path, bool has_header = false);  // function declaration for loading CSV files into Eigen::MatrixXd
+
+    std::vector <int> load_labels(const std::string& path, bool has_header = false); // returns a vector of labels corresponding to the transactions in csv file, 1 for fraud, 0 for legitimate, -1 for unknown
+
+    std::unordered_map<long long, int> build_txid_index(const std::string& path, bool has_header = false); //maps the id of transaction i.e long long txid to its corresponding row index in the csv file, returns an unordered_map of txid to row index
+
+    std::vector<std::pair<int,int>> load_edges(const std::string& path, const std::unordered_map<long long, int>& txid_to_row, bool has_header = false);
 }
--- src/data_loader.cpp
+++ src/data_loader.cpp
@@ -4,18 +4,25 @@
 #include <sstream>     // for string splitting
 #include <vector>      // stores values temporarily
 #include <stdexcept>
+#include <unordered_map> // for build_txid_index function
 
 using namespace std;
 
 namespace microtorch {
 
-Eigen::MatrixXd load_csv(const string& path)
+Eigen::MatrixXd load_csv(const string& path, bool has_header)
 {
     ifstream file(path);  // Open the file
 
     if (!file.is_open())
     {
         throw runtime_error("Could not open file");
+    }
+
+    if(has_header)
+    {
+        string header_line;
+        getline(file, header_line);  // Read and discard the header line if csv file has a header
     }
 
     string line;
@@ -60,6 +67,127 @@ Eigen::MatrixXd load_csv(const string& path)
 
 }
 
+vector <int> load_labels(const string& path, bool has_header)
+{
+    ifstream file(path);  // Open the file
+    if (!file.is_open())
+    {
+        throw runtime_error("Could not open file");
+    }
+
+    if(has_header)
+    {
+        string header_line;
+        getline(file, header_line);  // Read and discard the header line if csv file has a header
+    }
+
+    vector<int> labels;  // to store the labels
+    string line;
+    
+
+    while(getline(file, line))
+    {
+        stringstream ss(line); // convert each line to kind of file to read sth at each interval like value by value
+        string txid_text, class_text; // to store the two values of each line 
+
+        getline(ss, txid_text, ','); // read till comma and store in txid_text
+        getline(ss, class_text, ','); // read the second value of each line and store in class_text
+
+        int label; 
+
+        if (class_text == "1") {
+            label = 1;  // if transaction is illicit/fraudulent, label = 1
+        }
+
+        else if(class_text == "2"){
+            label = 0;  // if transaction is licit/legitimate, label = 2
+        }
+
+        else{
+            label = -1; // if the transaction type is unknown, its label is assigned -1
+        }
+
+        labels.push_back(label); // store the label in the vector labels
+
+    }
+
+    return labels; 
+}
+
+unordered_map<long long, int> build_txid_index(const string& path, bool has_header)
+{
+    ifstream file(path);
+
+    if (!file.is_open())
+    {
+        throw runtime_error("Could not open file");
+    }
+
+    if (has_header)
+    {
+        string header_line;
+        getline(file, header_line);
+    }
+
+    unordered_map<long long, int> index;
+    string line;
+    int row = 0;   //counts the row index starting from 0
+
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        string txid_text;
+
+        getline(ss, txid_text, ',');   // only take the first value i.e txid
+
+        long long txid = stoll(txid_text);   // convert to a number
+
+        index[txid] = row;  // map the txid to its corresponding row index in the csv file
+
+        row = row + 1;   // move to next row for the next iteration
+    }
+
+    return index;
+}
+
+vector<pair<int,int>> load_edges(const string& path, const unordered_map<long long, int>& txid_to_row, bool has_header)
+{
+    ifstream file(path);
+
+    if (!file.is_open())
+    {
+        throw runtime_error("Could not open file");
+    }
+
+    if (has_header)
+    {
+        string header_line;
+        getline(file, header_line);  //bypass the header line if csv file has one
+    }
+
+    vector<pair<int,int>> edges;  // pair means storing in (1,2) this format
+    string line;
+
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        string txid1_text, txid2_text;
+
+        getline(ss, txid1_text, ','); // first value of each line is txid1
+        getline(ss, txid2_text, ','); // second value of each line is txid2
+
+        long long txid1 = stoll(txid1_text); // convert to number
+        long long txid2 = stoll(txid2_text); // convert to number
+
+        int row1 = txid_to_row.at(txid1); // get the row index of txid1 from the unordered_map
+        int row2 = txid_to_row.at(txid2); // get the row index of txid2 from the unordered_map
+
+        // 4. push_back a pair {row1, row2} into edges
+        edges.push_back({row1, row2});
+    }
+
+    return edges;
+}
 }
 
 
