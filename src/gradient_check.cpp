3682b22d824206a1b82c17ce6beadf4772e4e9d3
utsabrajbhattarai <utsabdai000@gmail.com>
2026-07-13 21:26:21 +0545

    Feat: Added analytic gradient

--- src/gradient_check.cpp
+++ src/gradient_check.cpp
@@ -1,1 +1,18 @@
 #include "microtorch/gradient_check.hpp"
+
+namespace microtorch{
+
+    double gradient_check(std::function<TensorPtr(TensorPtr)> loss_fn, const TensorPtr& input, double eps = 1e-6){
+
+        //Zeroing the input gradient just in case user passed with non-zeroed values:
+        input->zero_grad();
+
+        //Computing the analytic gradient
+        TensorPtr loss = loss_fn(input);
+        loss->backward();
+        Eigen::MatrixXd analytic_gradient = input->grad;    //copying the input gradients for further comparision later on
+
+
+    }
+
+}
