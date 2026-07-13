2f9d1b6261fba848f7a1f2e21427d357164a71c0
utsabrajbhattarai <utsabdai000@gmail.com>
2026-07-13 20:36:20 +0545

    Feat: Added gradient_check.hpp and its function structure

--- include/microtorch/gradient_check.hpp
+++ include/microtorch/gradient_check.hpp
@@ -1,1 +1,13 @@
-#TODO
+#pragma once
+#include "microtorch/Tensor.hpp"
+#include <functional>
+
+namespace microtorch {
+
+    //To check if the gradients are not silently breaking down by comparing analytic gradient with the numerical approx.
+    //returns max difference between the gradients mentioned
+    double gradient_check(std::function<TensorPtr(TensorPtr)> loss_fn,  //Wrapper that is basically lamda replication of whole forward() for any list of operations for eg (Tensorptr x) {return func(list of args);}
+                        const TensorPtr& input,                         //input tensors whose gradient we need to check
+                        double eps =1e-6);                              //approx for derivatives                       
+
+}
--- src/gradient_check.cpp
+++ src/gradient_check.cpp
@@ -1,3 +1,1 @@
-#TODO: Implement a single function that runs the forward and backward once to get analytic gradient
-#runs the forward only many times nudging one input lements ata a time to build numeric gradient
-#return how far apart the gradient from engine/ops and engine/harness
+#include "microtorch/gradient_check.hpp"
