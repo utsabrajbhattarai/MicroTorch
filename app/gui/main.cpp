89bab13e17885f0a15ea5cc0275af6659a621c96
Shubham Pokhrel <shubhampokhrel64@gmail.com>
2026-07-30 18:36:09 +0545

    Set up raylib, confirm window opens

--- app/gui/main.cpp
+++ app/gui/main.cpp
@@ -1,1 +1,16 @@
-'''TODO- Implement GUI interface on this file'''
+#include "raylib.h"
+
+int main() {
+    InitWindow(1280, 720, "MicroTorch GNN Dashboard");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, raylib!", 20, 20, 20, BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
