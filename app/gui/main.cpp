02c6b2fe5e2ddf047c799a3ea8e2420414fd6000
Shubham Pokhrel <shubhampokhrel64@gmail.com>
2026-08-01 07:42:00 +0545

    Draw loaded nodes as colored circles in a temporary grid layout

--- app/gui/main.cpp
+++ app/gui/main.cpp
@@ -5,9 +5,6 @@
 int main() {
     auto nodes = load_nodes_csv("gui_artifacts/nodes.csv");
     std::cout << "Loaded " << nodes.size() << " nodes\n";
-    for (const auto& n : nodes) {
-        std::cout << n.node_id << " " << n.account_id << " " << n.pred_prob << "\n";
-    }
 
     InitWindow(1280, 720, "MicroTorch GNN Dashboard");
     SetTargetFPS(60);
@@ -15,7 +12,22 @@ int main() {
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello, raylib!", 20, 20, 20, BLACK);
+
+        // temporary: draw each node as a circle in a simple grid, just to see the data
+        int x = 100;
+        int y = 100;
+        for (size_t i = 0; i < nodes.size(); ++i) {
+            Color c = (nodes[i].pred_label == 1) ? RED : GREEN;
+            DrawCircle(x, y, 20, c);
+            DrawText(TextFormat("%lld", nodes[i].account_id), x - 30, y + 25, 10, BLACK);
+
+            x += 150;
+            if (x > 1100) {
+                x = 100;
+                y += 150;
+            }
+        }
+
         EndDrawing();
     }
 
