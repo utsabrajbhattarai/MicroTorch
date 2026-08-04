8677d9a9afd7a6f008036263c566dd7884821b61
utsabrajbhattarai <utsabdai000@gmail.com>
2026-08-04 22:14:21 +0545

    Feat: Implemented the training loop

--- app/diffusion_train.cpp
+++ app/diffusion_train.cpp
@@ -14,9 +14,9 @@ using namespace microtorch;
 const int N = 1000; //number of points
 const int T = 200; //number of timestamps
 const int hidden = 64;  //number of hidden neurons
-const double lr = 0.001;    //learning rate of adam optimizer
+const double lr = 0.0005;    //learning rate of adam optimizer
 
-
+const int MAX_EPOCH = 250;
 
 int main() {
     
@@ -35,6 +35,45 @@ int main() {
     //generator for timstep pick and forward noise
     std::mt19937 rng(95);
 
+    //uniform distribution for noise t at each epoch
+    std::uniform_int_distribution<int> t_dist(0, T - 1);
+
+    //training loop:
+    for (int epoch = 0; epoch <= MAX_EPOCH; epoch++){
+
+        
+        int t = t_dist(rng); //each epoch => random noise
+        Eigen::MatrixXd noise_out; //training target
+        Eigen::MatrixXd x_t = forward_noise(x0, t, ns, noise_out, rng); //filling noise_out and generating a noised x_t
+
+
+        //making the input_tensor
+        Eigen::MatrixXd input(N,3);
+        input.leftCols(2) = x_t;    //filling inputs first 2 columns with noised x_t
+        input.col(2).setConstant(t / static_cast<double>(T));   //normalizing t to make it range from 0 to 1
+        TensorPtr input_tensor = make_tensor(input);
+
+        TensorPtr pred = model.forward(input_tensor); //getting prediction using the forward layer
+
+        TensorPtr loss = mse_loss(pred, noise_out); //comparing predicted with real noise using mse
+
+        //BACKWARD + UPDATE WEIGHTS AND BIASES:
+        opt.zero_grad(); //resetting gradient 0
+
+        loss->backward(); //backward through all parameters
+
+        opt.step(); //updating gradient using adam
+
+
+        //print epoch::
+        if (epoch % 10 == 0){
+            std::cout << "epoch " << epoch << "  loss " << loss->data(0,0) << std::endl;
+        }
+
+    }
+
+
+    
 
     return 0;
 }
